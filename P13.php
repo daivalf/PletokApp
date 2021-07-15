6d426ba8a2fd3856cf0b8b74b09405d58329cc63
ridhamad <87405864+ridhamad@users.noreply.github.com>
2021-07-15 20:16:24 +0700

    Add files via upload

---
+++ P13.php
@@ -0,0 +1,47 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <title>Bir Pletok P023</title>
+  <link rel="stylesheet" type="text/css" href="style12.css">
+  
+  <style>
+	@import url('https://fonts.googleapis.com/css2?family=Montserrat:wght@500&family=Pacifico&display=swap');	
+  </style>
+</head>
+<body>
+<div class="container">
+		<div class="navbar">
+			<div class="judul">PLETOK APP</div>
+			<ul>
+				<li><a href="#">Back</a></li>	
+				<div class ="clear"></div>
+			</ul>
+		</div>
+	<div class="background"></div>
+			<div class="konfirmasi13">Laporan Pendapatan </div>
+	<div class="kotak13">
+
+			<div class="tabel13">
+				<table border="1">
+					<tr><th>ID Pembayaran</th><th>Total</th>
+					<tr><td>&nbsp;</td><td>&nbsp;</td>
+					<tr><td>&nbsp;</td><td>&nbsp;</td>
+					<tr><td>&nbsp;</td><td>&nbsp;</td>
+					<tr><td>&nbsp;</td><td>&nbsp;</td>
+					<tr><td>&nbsp;</td><td>&nbsp;</td>
+					<tr><td>&nbsp;</td><td>&nbsp;</td>
+					<tr><td>&nbsp;</td><td>&nbsp;</td>
+					<tr><td>&nbsp;</td><td>&nbsp;</td>
+					<tr><td>&nbsp;</td><td>&nbsp;</td>
+					<tr><td>&nbsp;</td><td>&nbsp;</td>
+					<tr><td>&nbsp;</td><td>&nbsp;</td>
+					<tr><td>&nbsp;</td><td>&nbsp;</td>
+					<tr><td>&nbsp;</td><td>&nbsp;</td>
+
+				</table>
+			<div class="tambah13" align=><a href ="#">Submit Laporan Pendapatan</a></div>			
+			</div>
+	</div>
+</div>	
+</body>
+</html>
---
+++ P16.php
@@ -0,0 +1,51 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <title>Bir Pletok P023</title>
+  <link rel="stylesheet" type="text/css" href="style12.css">
+  
+  <style>
+	@import url('https://fonts.googleapis.com/css2?family=Montserrat:wght@500&family=Pacifico&display=swap');	
+  </style>
+</head>
+<body>
+<div class="container">
+		<div class="navbar">
+			<div class="judul">PLETOK APP</div>
+			<ul>
+				<li><a href="#">Back</a></li>	
+				<div class ="clear"></div>
+			</ul>
+		</div>
+	<div class="background"></div>
+			<div class="konfirmasi16">Laporan Pendapatan </div>
+	<div class="kotak13">
+		<select name="bulan" class="bulan">
+			<option>Pilih Bulan</option>
+			<option value="januari">Januari</option>
+			<option value="Februari">Februari</option>
+			<option value="Maret">Maret</option>
+			<option value="April">April</option>
+		</select>
+		
+		<select name="tahun" class="tahun">
+			<option>Pilih Tahun</option>
+			<option value="2018">2018</option>
+			<option value="2019">2019</option>
+			<option value="2020">2020</option>
+			<option value="2021">2021</option>
+		</select>
+		
+			<div class="tabel16">
+				<table border="1">
+					<tr><th>Tanggal</th><th>Total</th>
+					<tr><td>&nbsp;</td><td>&nbsp;</td>
+					<tr><td>&nbsp;</td><td>&nbsp;</td>
+					<tr><td>&nbsp;</td><td>&nbsp;</td>
+
+				</table>		
+			</div>
+	</div>
+</div>	
+</body>
+</html>
