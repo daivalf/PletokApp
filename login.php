3dd3fcef9c50500a6c195d540794d8a675a00c4f
daivalf <87405620+daivalf@users.noreply.github.com>
2021-07-17 16:10:16 +0700

    Add files via upload

--- login.php
+++ login.php
@@ -7,7 +7,7 @@
     {
         $id_pegawai = $db->escape_string($_POST["id_pegawai"]);
         $password = $db->escape_string($_POST["password"]);
-        $sql = "SELECT id_pegawai, nama_pegawai
+        $sql = "SELECT id_pegawai, nama_pegawai, jabatan
                 FROM tb_pegawai
                 WHERE id_pegawai = '$id_pegawai' and password = '$password'";
         $res = $db->query($sql);
@@ -19,7 +19,26 @@
                 session_start();
                 $_SESSION["id_pegawai"] = $data["id_pegawai"];
                 $_SESSION["nama_pegawai"] = $data["nama_pegawai"];
-                header("Location: P02.php");
+                $_SESSION["jabatan"] = $data["jabatan"];
+                if ($_SESSION["jabatan"] == "Pelayan")
+                {
+                    header("Location: P02.php");
+                }
+                else
+                if ($_SESSION["jabatan"] == "Bartender") 
+                {
+                    header("Location: P03.php");
+                }
+                else
+                if ($_SESSION["jabatan"] == "Kasir") 
+                {
+                    header("Location: P04.php");
+                }
+                else
+                if ($_SESSION["jabatan"] == "Owner")
+                {
+                    header("Location: P05.php");
+                }
             }
             else 
             {
