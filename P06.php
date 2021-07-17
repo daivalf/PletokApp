14669f3df2cbb7c3f3618cddc0904975743ae943
fauzanhilmy27 <87411579+fauzanhilmy27@users.noreply.github.com>
2021-07-17 11:47:05 +0700

    Add files via upload

--- P06.php
+++ P06.php
@@ -1,5 +1,7 @@
 <?php
-include_once("functions.php");?>
+include_once("functions.php");
+$db=dbconnect();
+?>
 <!DOCTYPE html>
 <html>
 <head>
@@ -11,30 +13,53 @@ include_once("functions.php");?>
     <body>
         <div class="background"></div>
     <h1 class="h1">Daftar meja : </h1>
+    <form method="post" name="F" action="P06-update.php">
+    <?php
+    $db = dbConnect();
+    if($db->connect_errno==0){
+        $sql = "SELECT tb_meja.nomor_meja,tb_meja.status
+        FROM tb_meja
+        ";
+         if (isset($_POST["cari"])) {
+            $sql = cari_jenis($_POST["keyword"]);
+        }
+        $res= $db->query($sql);
+        if($res) {
+    ?>
         <table border="1" class="table">
         <tr>
             <th>Nomor Meja</th>
             <th>Status</th>
         </tr>
+        <?php
+   $data=$res->fetch_all(MYSQLI_ASSOC);
+    foreach($data as $barisdata) {
+?>
         <tr>
-            <td>01</td>
-            <td>Tersedia</td>
+            <td><?php echo $barisdata["nomor_meja"]?></td>
+            <td>
+                <select name="status">
+                <option value="tersedia">Tersedia</option>
+                <option value="tidaktersedia">Tidak Tersedia</option>
+            </select></td>
         </tr>
-        <tr>
-            <td>02</td>
-            <td>Tidak Tersedia</td>
-        </tr>
-        <tr>
-            <td>03</td>
-            <td>Tersedia</td>
-        </tr>
-        <tr>
-            <td>04</td>
-            <td>Tersedia</td>
-        </tr>
+            <?php
+    }
+?>
+            
+	
         </table>
-        
         <h1></h1>
-        <button class="btn" href="M02.php">save</button>
-        <button class="btn">reset</button>
+        <input type="submit" name="tblsimpan" value="simpan" class="btn">
+        <input type="reset"class="btn">
+    </form>
     </body>
+</html>
+<?php
+        }
+        else
+        echo"Error : ".$db->error."<br>"; 
+        }
+    else
+    echo"Error koneksi db";
+        ?>
